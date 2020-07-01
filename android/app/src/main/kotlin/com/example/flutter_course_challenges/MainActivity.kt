40bccec34b36954e7a1f2832a3b0c4ad30c418ec
Tane10 <16818483+Tane10@users.noreply.github.com>
2020-07-01 15:14:51 +0100

    completed my info card

--- android/app/src/main/kotlin/com/example/flutter_course_challenges/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.flutter_course_challenges
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,72 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Center(
-            child: Text("I\'m poor man"),
-          ),
-          backgroundColor: Colors.blueGrey[700],
-        ),
-        body: Center(
-          child: Image(image: AssetImage('images/coal.png')),
-        ),
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CircleAvatar(
+              radius: 50,
+              backgroundColor: Colors.red,
+              backgroundImage: AssetImage('images/joshFace.jpeg'),
+            ),
+            Text("Joshua Thomas",
+                style: TextStyle(
+                    fontSize: 40,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Pacifico')),
+            Text(
+              "Full Stack Developer",
+              style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.teal[100],
+                  fontFamily: "Lato-Bold",
+                  letterSpacing: 1.5),
+            ),
+            SizedBox(
+                height: 20,
+                width: 150,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                )),
+            Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(Icons.phone, color: Colors.teal),
+                  title: Text(
+                    "+44 7931104349",
+                    style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: "Lato-Bold",
+                        fontSize: 20),
+                  ),
+                )),
+            Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                    leading: Icon(Icons.email, color: Colors.teal),
+                    title: Text(
+                      "j.thomas.houlker@gmail.com",
+                      style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontFamily: "Lato-Bold",
+                          fontSize: 20),
+                    )))
+          ],
+        )),
       ),
-    ),
-  );
+    );
+  }
 }
